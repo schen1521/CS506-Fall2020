1c951b05f52d4f6291f354f6b8c39f041bd5e5fc
Shunying Chen <schen000@bu.edu>
2020-09-14 11:20:30 -0500

    added a tree, and addressed feedback

--- 01-python/infrastructure/tree.py
+++ 01-python/infrastructure/tree.py
@@ -1,3 +1,12 @@
-def draw_tree():
-    print("tree not found")
+def draw_tree(height=3):
+    print("   _____   ")
+    print("  / ~   \\")
+    print(" /       \\")
+    print(" \\    ~  /")
+    print("  \\_   _/")
+    for i in range(height):
+    	print("    | |   ")
+    print("   /   \\  ")
+    print("///////////")
+    print()
     return
