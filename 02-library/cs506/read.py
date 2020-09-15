548b8f715dbc22cd4dd119d8fd0bc15efef5104a
Shunying Chen <schen000@bu.edu>
2020-09-15 16:16:42 -0500

    Completed 02-library

--- 02-library/cs506/read.py
+++ 02-library/cs506/read.py
@@ -3,4 +3,10 @@ def read_csv(csv_file_path):
         Given a path to a csv file, return a matrix (list of lists)
         in row major.
     """
-    raise NotImplementedError()
+    res = []
+    with open(csv_file_path, "r") as f:
+    	lines = f.readlines()
+    	for line in lines:
+    		line = line.strip("\n").replace('"','')
+    		res.append([int(x) if x.isdigit() else str(x) for x in line.split(",")])
+    return res
